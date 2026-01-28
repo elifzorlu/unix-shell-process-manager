9325f4504be1d8c1b05fdb25d8879dd84812e51c
Elif Zorlu <147428324+elifzorlu@users.noreply.github.com>
2026-01-28 16:38:58 -0500

    Add main function to run Shell

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "Shell.h"
+
+int main(int argc, char **argv) {
+    (void)argc; // Silence unused variable warning
+    (void)argv;
+
+    Shell myShell;
+    myShell.run();
+
+    return 0;
+}
